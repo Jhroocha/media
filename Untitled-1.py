25c42832be5d8134006d2c33975fdbb260079dc7
Jhroocha <128037139+Jhroocha@users.noreply.github.com>
2024-09-20 11:38:47 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,25 @@
+
+print("Olá, seja bem vindo(a)!")
+nome = input("Digite seu nome:")
+print("Prazer,"  +  nome)
+print("Ok, agora vamos precisar das suas notas para calcularmos a sua média")
+P1 = int(input("Digite a nota da sua P1:"))
+P2 = int(input("Digite a nota da sua P2:"))
+P3 = int(input("Digite a nota da sua P3:"))
+media = ( P1 +P2 + P3)/3
+
+
+if media >=7:
+   print("Parabéns, você passou de semestre!")
+
+else: 
+ print("Que pena, você reprovou...")
+
+
+
+
+
+
+
+
+

c2fd05b7067d7e32d39ac5d105ebd8905b3a9061
Jhroocha <128037139+Jhroocha@users.noreply.github.com>
2024-11-25 13:38:21 -0300

    Add files via upload

---
+++ MÉDIA.py
@@ -0,0 +1,29 @@
+
+print("Olá, seja bem vindo(a)!")
+nome = input("Digite seu nome:")
+print("Prazer,"  +  nome)
+print("Ok, agora vamos precisar das suas notas para calcularmos a sua média")
+P1 = float(input("Digite a nota da sua P1:"))
+P2 = float(input("Digite a nota da sua P2:"))
+P3 = float(input("Digite a nota da sua P3:"))
+media = ( P1 +P2 + P3)/3
+print("Sua média é:", media)
+
+
+if media >=7:
+   
+   
+   print("Parabéns! Você passou")
+
+else: 
+
+ print("Que pena... Você reprovou")
+
+
+
+
+
+
+
+
+
